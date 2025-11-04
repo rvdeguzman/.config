ec10f935372d399f65554d4779bec4f715a9fbb9
rv <rvdeguzman@protonmail.com>
2025-11-03 21:14:56 -0500

    modern roam

--- doom/config.el
+++ doom/config.el
@@ -6,49 +6,48 @@
 (setq doom-font (font-spec :family "Iosevka Nerd Font Mono" :size 14))
 (setq display-line-numbers-type 'relative)
 
-;; Org paths (set these before org loads)
 (setq org-directory "~/org"
-      org-roam-directory (file-truename "~/org/roam"))
+      org-roam-directory (file-truename "~/org/roam")
+      org-agenda-files (list (file-truename "~/org/roam/dailies")))
 
-;; Optional: org-roam only handles .org in v2; no need to set this.
-;; (setq org-roam-file-extensions '("org"))
-
-;; Display template: title + tags (avoid ${type:15})
 (setq org-roam-node-display-template
       (concat "${title:*} " (propertize "${tags:10}" 'face 'org-tag)))
 
 (setq org-roam-capture-templates
-      '(("m" "main" plain "%?"
-         :if-new (file+head "main/${slug}.org" "#+title: ${title}\n")
+      '(("s" "subject" plain "%?"
+         :if-new (file+head "subjects/${title}.org" "#+title: ${title}\n#+filetags: :subject:\n")
+         :immediate-finish t :unnarrowed t)
+        ("c" "concept" plain "%?"
+         :if-new (file+head "concepts/${title}.org" "#+title: ${title}\n")
+         :immediate-finish t :unnarrowed t)
+        ("g" "goal" plain "%?"
+         :if-new (file+head "goals/${title}.org" "#+title: ${title}\n#+filetags: :goal:\n")
          :immediate-finish t :unnarrowed t)
         ("r" "reference" plain "%?"
          :if-new (file+head "reference/${title}.org" "#+title: ${title}\n")
-         :immediate-finish t :unnarrowed t)
-        ("a" "article" plain "%?"
-         :if-new (file+head "articles/${title}.org"
-                            "#+title: ${title}\n#+filetags: :article:\n")
-         :immediate-finish t :unnarrowed t)
-        ("d" "default" plain "%?"
-         :if-new (file+head "%<%Y%m%d%H%M%S>-${slug}.org" "#+title: ${title}\n")
-         :unnarrowed t)))
+         :immediate-finish t :unnarrowed t)))
+
+;; org-roam-dailies templates
+(setq org-roam-dailies-capture-templates
+      '(("d" "default" entry "** Habits\n- [ ] Fasting\n- [ ] Exercise\n- [ ] Work\n\n** Timesheet\n| Task | Hours | Notes |\n|------|-------|-------|\n|      |       |       |\n\n** Notes\n%?"
+         :target (file+head "%<%Y-%m-%d>.org" "#+title: %<%Y-%m-%d>\n#+filetags: :daily:\n"))))
 
 ;; org-roam
 (use-package! org-roam
   :custom
   (org-roam-directory (file-truename "~/org/roam"))
-  :bind (("C-c n l" . org-roam-buffer-toggle)
-         ("C-c n f" . org-roam-node-find)
-         ("C-c n g" . org-roam-graph)
-         ("C-c n i" . org-roam-node-insert)
-         ("C-c n c" . org-roam-capture)
-         ("C-c n j" . org-roam-dailies-capture-today))
+   :bind (("C-c n l" . org-roam-buffer-toggle)
+          ("C-c n f" . org-roam-node-find)
+          ("C-c n g" . org-roam-graph)
+          ("C-c n i" . org-roam-node-insert)
+          ("C-c n c" . org-roam-capture)
+          ("C-c n d" . org-roam-dailies-capture-today))
   :config
   (setq org-roam-node-display-template
         (concat "${title:*} " (propertize "${tags:10}" 'face 'org-tag)))
   (org-roam-db-autosync-mode)
   (require 'org-roam-protocol))
 
-;; org-roam-ui (ensure `websocket` & `org-roam-ui` are in packages.el)
 (use-package! org-roam-ui
   :after org-roam
   :config
@@ -56,7 +55,6 @@
         org-roam-ui-follow t
         org-roam-ui-update-on-save t))
 
-;; vterm (usually provided by :term vterm in Doom; keep only if you need it)
 (use-package! vterm
   :commands vterm)
 
@@ -74,7 +72,8 @@
         org-download-image-dir "./images"
         org-download-heading-lvl nil
         org-download-timestamp "_%Y%m%d_%H%M%S"
-        org-download-annotate-function (lambda (_link) ""))
+        org-download-annotate-function (lambda (_link) "")
+        org-download-screenshot-method "grimblast save area %s"))
   :bind (:map org-mode-map
          ("C-c i v" . org-download-clipboard)))
 (defun org-roam-capture-here ()
@@ -95,4 +94,4 @@
 (map! :leader
       :prefix "w"
       "h" #'evil-window-split
-      "v" #'evil-window-vsplit)
+      "v" #'evil-window-vsplit)
