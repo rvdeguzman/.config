5299bfed72841101bf0ec0d45dc48144b79dd58e
rv <rvdeguzman@protonmail.com>
2026-03-30 13:16:37 -0400

    tmux config

--- tmux/layout.sh
+++ tmux/layout.sh
@@ -4,18 +4,21 @@
 
 SESSION="$(tmux new-session -d -n "main" -P -F '#{session_id}')"
 
-# Window 2: 1 left, 2 right stacked
-tmux new-window -t "$SESSION" -n "split"
+tmux new-window -t "$SESSION" -n "agents"
+tmux split-window -v -t "$SESSION"
+
+tmux send-keys -t "$SESSION:1.0" 'nvim' Enter
+
+tmux send-keys -t "$SESSION:2.0" 'claude' Enter
+tmux send-keys -t "$SESSION:2.1" 'codex' Enter
+
+
+tmux new-window -t "$SESSION" -n "server"
 tmux split-window -h -t "$SESSION"
 tmux split-window -v -t "$SESSION"
 
-  tmux send-keys -t "$SESSION:1.0" 'nvim' Enter
+tmux send-keys -t "$SESSION:3.2" 'lazygit' Enter
 
-  # Window 2
-  tmux send-keys -t "$SESSION:2.0" 'claude' Enter
-  tmux send-keys -t "$SESSION:2.2" 'lazygit' Enter
-
-# Focus window 1, left pane
 tmux select-window -t "$SESSION:1"
 tmux select-pane -t "$SESSION" -L
 
